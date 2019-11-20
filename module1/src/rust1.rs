4ff53f8e247e11cb4364cfcabe99abd7c607d85f
Andy Bell <andy.bell@displaynote.com>
2019-11-20 11:44:28 +0100

    operators, casting with 'as T'

--- module1/src/rust1.rs
+++ module1/src/rust1.rs
@@ -1,5 +1,5 @@
 use std::mem;
-
+use std;
 fn foo_function() {
     let y = 5;
     println!("foo_function is being called with result is {}", y);
@@ -45,4 +45,27 @@ pub fn main1() {
     // Shadow
     let scoped_var = 15;
     println!("Redeclared scoped_var={} and stack={} and stack_mut={}", scoped_var, stack, stack_mut);
+
+    // operators
+
+    let number = 2;
+    let add_number = number + 5;
+    println!("add_number={}", add_number);
+
+    let mut another_number = 7;
+    another_number -= 2;
+    println!("another_number = {}", another_number);
+
+    let cubed = i32::pow(number, 3);
+
+    println!("number raised to the power 3={}", cubed);
+
+    // We had to cast using 'as f64' here
+    let cubed_times_pi = cubed as f64 * std::f64::consts::PI;
+    println!("cubed_times_PI={}", cubed_times_pi);
+    
+    let bool_result = 5 < 6;
+    println!("bool_result={}", bool_result);
 }
+
+
